ee6df27115bbe00556cf311056e8c72a0953f685
Ayush Vyas <rajvyasayush@gmail.com>
2018-07-27 00:14:14 +0530

    Initial commit

---
+++ index.php
@@ -0,0 +1,320 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>OneClick a Mobile App which provides you what you want.. | Home :: w3layouts</title>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="keywords" content="Appload Responsive web template, Bootstrap Web Templates, Flat Web Templates, Andriod Compatible web template, 
+Smartphone Compatible web template, free webdesigns for Nokia, Samsung, LG, SonyErricsson, Motorola web design" />
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<link href="css/style.css" rel="stylesheet" type="text/css" media="all" />
+<link href="css/bootstrap.css" rel="stylesheet" type="text/css" media="all">
+<link href="css/styles.css?v=1.6" rel="stylesheet">
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
+
+
+<!-- js -->
+<script src="js/jquery.min.js"></script>
+<script src="js/scripts.js?v=1.7"></script>
+<!-- //js -->
+<!-- start-smoth-scrolling -->
+<script type="text/javascript" src="js/move-top.js"></script>
+<script type="text/javascript" src="js/easing.js"></script>
+<script type="text/javascript">
+	jQuery(document).ready(function($) {
+		$(".scroll").click(	{		
+			event.preventDefault();
+			$('html,body').animate({scrollTop:$(this.hash).offset().top},1000);
+		});
+	});
+</script>
+<!-- start-smoth-scrolling -->
+</head>
+	<body class="cbp-spmenu-push">
+		<!-- header -->
+		<div class="header-bg">
+					<section class="navigation">
+							<div class="container-fluid">
+								<div class="col-md-8 col-md-offset-2 col-sm-8 col-sm-offset-2 col-xs-8 col-xs-offset-2">
+								  <nav class="pull">
+									<ul class="nav">
+									  <li><a href="#home" class="scroll">Home</a></li>
+									  <li><a href="#about" class="scroll">About</a></li>
+									  <li><a href="#services" class="scroll">Services</a></li>
+									  <li><a href="#features" class="scroll">Features</a></li>
+									   <li><a href="#pricing" class="scroll">Pricing</a></li>
+									    <li><a href="#mail" class="scroll">Mail us</a></li>
+									</ul>
+								  </nav>
+								</div>
+							</div>
+						</section>
+						<div class="header-top">
+							<div class="container">
+								<div class="head-logo">
+									
+								</div>
+								<div class="top-nav">
+									<section class="hero fa-navicon fa-2x nav_slide_button" id="hero">
+										<a href="#"><img src="images/menu-icon.png" alt=""></a>
+									</section>	
+								</div>	
+								<div class="clearfix"> </div>
+							</div>
+						</div>
+			<div class="banner-info">
+				<!-- container -->
+				<div class="container">
+					<div class="col-md-5 slid">
+						<!--- banner Slider starts Here --->
+							<script src="js/responsiveslides.min.js"></script>
+						 <script>
+							// You can also use "$(window).load(function() {"
+							$(function () {
+							  // Slideshow 4
+							  $("#slider4").responsiveSlides({
+								auto: true,
+								pager: true,
+								nav: true,
+								speed: 500,
+								namespace: "callbacks",
+								before: function () {
+								  $('.events').append("<li>before event fired.</li>");
+								},
+								after: function () {
+								  $('.events').append("<li>after event fired.</li>");
+								}
+							  });
+						
+							});
+						  </script>
+						<!----//End-slider-script---->
+						<div  id="top" class="callbacks_container">
+							<ul class="rslides" id="slider4">
+								<li>
+									<div class="mobile-device">
+										<img src="images/OneTap.png" alt="" />
+									</div>
+								</li>
+								
+							</ul>
+						</div>
+					</div>
+					<div class="col-md-7 banner-info-left">
+						<h2>OneTap India's best searching plateform, provides Best Deals, Hotels, Movies, Buses and Cabs.</h2>
+						<p>It provides search services and is made available on Website and Mobile.</p>
+						<div class="banner-buttons">
+							<div class="banner-button">
+								<a href="#"><img src="images/1.png" alt="" /></a>
+							</div>
+							<div class="banner-button green-button">
+								<a href="#"><img src="images/2.png" alt="" /></a>
+							</div>
+							<div class="clearfix"> </div>
+						</div>
+					</div>
+					<div class="clearfix"> </div>
+				</div>
+				<!-- //container -->
+			</div>
+		</div>
+		<!-- //header -->
+		<!-- banner-bottom -->
+		<div id="services" class="banner-bottom">
+			<!-- container -->
+			<div class="container">
+				<div class="banner-text">
+					<h3 style="color: gray;">Services</h3>
+					<p>It provide fast, free, reliable and comprehensive information to the users and connect buyers to sellers.</p>
+				</div>
+				<div class="banner-bottom-grids">
+					<div class="col-md-4 banner-bottom-grid">
+						<div class="services-icon">
+							<span class="glyphicon glyphicon-th-large" aria-hidden="true"></span>
+						</div>
+						<h4>Best Searching plateform</h4>
+						
+					</div>
+					<div class="col-md-4 banner-bottom-grid red">
+						<div class="services-icon">
+							<span class="glyphicon glyphicon-th-large glyphicon-phone" aria-hidden="true"></span>
+						</div>
+						<h4>One tap serving</h4>
+						
+					</div>
+					<div class="col-md-4 banner-bottom-grid green">
+						<div class="services-icon">
+							<span class="glyphicon glyphicon-th-large glyphicon-thumbs-up" aria-hidden="true"></span>
+						</div>
+						<h4>Smart user interface</h4>
+						
+					</div>
+					<div class="clearfix"> </div>
+				</div>	
+			</div>
+			<!-- //container -->
+		</div>
+		<!-- //banner-bottom -->
+		<!-- about -->
+		<div id="about" class="about">
+			<!-- container -->
+			<div class="container">
+				<div class="about-info">
+					<h3  style="color: gray;">About Us</h3>
+					<p class="caption">OneTap is a Local Search engine that provides local search related services to users across India through multiple platforms such as website, mobile website, Apps (Android, iOS, Windows), over the telephone  and text (SMS).</p>
+					
+				</div>
+			</div>	
+		</div>
+		
+		<div id="features" class="features">
+			
+			<div class="container">
+				<div class="features-info">
+					<h3  style="color: gray;"	>Features</h3>
+					<p>Provide Informations about the nearby places like Restaurant, Hotels  and Shops.It is compatible with the  	</p>
+				</div>
+				<div class="features-grids">
+					<div class="col-md-7 features-right">
+						<h4>Different types of features available here :
+						</h4>
+						<div class="features-right-grids">
+							<div class="features-right-grid right-top">
+								
+								<div class="design-right">
+									<h5>Best Searh Engine</h5>
+									<p>It provides a best user search engine.</p>
+								</div>
+								<div class="clearfix"> </div>
+							</div>
+							<div class="features-right-grid right-middle-top">
+								
+								<div class="design-right">
+									<h5>Real Time tracking system</h5>
+									<p>It gives a real time tracking system of shop, restaurents and others.</p>
+								</div>
+								<div class="clearfix"> </div>
+							</div>
+							<div class="features-right-grid right-middle">
+								<div class="design-right">
+									<h5>Smart Chat Assistance</h5>
+									<p>It provide a live chat assistance. </p>
+								</div>
+								<div class="clearfix"> </div>
+							</div>
+							
+						</div>
+					</div>
+					<div class="col-md-5 slid">
+							<!--- banner Slider starts Here --->
+							 <script>
+								// You can also use "$(window).load(function() {"
+								$(function () {
+								  // Slideshow 4
+								  $("#slider3").responsiveSlides({
+									auto: true,
+									pager: true,
+									nav: true,
+									speed: 500,
+									namespace: "callbacks",
+									before: function () {
+									  $('.events').append("<li>before event fired.</li>");
+									},
+									after: function () {
+									  $('.events').append("<li>after event fired.</li>");
+									}
+								  });
+							
+								});
+							  </script>
+							<!----//End-slider-script---->
+							<div  id="top" class="callbacks_container">
+								<ul class="rslides" id="slider3">
+									<li>
+										<div class="mobile-device features-device">
+											<img src="images/OneTap.png" alt="" />
+										</div>
+									</li>
+									
+								</ul>
+							</div>
+					</div>
+					<div class="clearfix"> </div>
+				</div>
+			</div>
+			<!-- //container -->
+		</div>
+		<!-- //features -->
+		<!-- plans -->
+		
+		<!-- mail -->
+		<div id="mail" class="mail">
+			<!-- container -->
+			<div class="container">
+				<div class="mail-info">
+					<h3>Mail Us</h3>
+					
+				</div>
+				<div class="mail-grids">
+					<div class="col-md-6 mail-grid-left">
+						<h3>Address</h3>
+						
+						<h4>Headquarters</h4>
+						<p>Udaipur.
+							<span>Rajasthan</span>
+						</p>
+						<h4>Get In Touch</h4>
+						<p>Telephone: 7727927728
+							<span>FAX: +1 234 567 9871</span>
+							E-mail: <a href="mailto:onetap@email.com">onetap@gmail.com</a>
+						</p>
+					</div>
+					<div class="col-md-6 contact-text">
+						<form>
+							<input type="text" placeholder="Name" required="">
+							<input type="text" placeholder="Email" required="">
+							<input type="text" placeholder="Subject" required="">
+							<textarea placeholder="Message" required=""></textarea>
+							<input type="submit" value="SEND">
+						</form>
+					</div>
+					<div class="clearfix"> </div>
+				</div>
+			</div>
+			<!-- //container -->
+		</div>
+		<!-- //mail -->
+		<!-- footer -->
+		<div class="footer">
+			<!-- container -->
+			<div class="container">
+				<div class="footer-info">
+				<img src="images/gmail.png" height="50px" width="50px;" alt=""/>
+
+					
+				<div class="copyright">
+					<p style="color: gray;"> Copyright © 2018 OneTap. All rights reserved | Private Policy</p>
+				</div>
+			</div>
+		
+		</div>
+		<!-- //footer -->
+		<script type="text/javascript">
+									$(document).ready(function() {
+										/*
+										var defaults = {
+								  			containerID: 'toTop', // fading element id
+											containerHoverID: 'toTopHover', // fading element hover id
+											scrollSpeed: 1200,
+											easingType: 'linear' 
+								 		};
+										*/
+										
+										$().UItoTop({ easingType: 'easeOutQuart' });
+										
+									});
+								</script>
+									<a href="#" id="toTop" style="display: block;"> <span id="toTopHover" style="opacity: 1;"> </span></a>
+	<!-- content-Get-in-touch -->
+	</body>
+</html>
---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+
+$(window).load(function() {
+  $('.nav_slide_button').click(function() {
+    $('.pull').slideToggle();
+  });
+});
